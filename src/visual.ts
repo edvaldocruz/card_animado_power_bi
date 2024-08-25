7f3b6a2cacb9081b449aecb820de0e495c3d2df3
Edvaldo <edvaldo.sne@gmail.com>
2024-08-24 22:56:29 -0300

    circulos pelas laterais

--- src/visual.ts
+++ src/visual.ts
@@ -84,37 +84,36 @@ export class Visual implements IVisual {
 
         // Adicionar SVG com o valor da medida embutido
         const svgContent = `
-            <defs>
-                <clipPath id="clip0_1_55">
-                    <rect width="100%" height="100%" fill="white"/>
-                </clipPath>
-            </defs>
-
             <g clip-path="url(#clip0_1_55)">
-                <circle cx="41.4%" cy="50%" r="9.8%" stroke="#a3ecff" stroke-width="2.5%" fill="none">
-                    <animateTransform attributeName="transform" type="translate" from="0,0" to="-5.86%,-9.77%" dur="5s" begin="0s" repeatCount="indefinite" />
-                    <animate attributeName="r" from="9.8%" to="19.5%" dur="5s" begin="0s" repeatCount="indefinite" />
-                    <animate attributeName="opacity" from="1" to="0" dur="5s" begin="0s" repeatCount="indefinite" />
-                </circle>
-                <circle cx="54.7%" cy="59.4%" r="9.8%" stroke="#a4e3ff" stroke-width="2.1%" fill="none">
-                    <animateTransform attributeName="transform" type="translate" from="0,0" to="7.8%,-11.7%" dur="5s" begin="1s" repeatCount="indefinite" />
-                    <animate attributeName="r" from="9.8%" to="23.4%" dur="5s" begin="1s" repeatCount="indefinite" />
-                    <animate attributeName="opacity" from="1" to="0" dur="5s" begin="1s" repeatCount="indefinite" />
-                </circle>
-                <circle cx="50.4%" cy="67.2%" r="7.8%" stroke="#a3f9ff" stroke-width="1.8%" opacity="0.3433" fill="none">
-                    <animateTransform attributeName="transform" type="translate" from="0,0" to="-3.9%,-7.8%" dur="5s" begin="2s" repeatCount="indefinite" />
-                    <animate attributeName="r" from="7.8%" to="17.6%" dur="5s" begin="2s" repeatCount="indefinite" />
-                    <animate attributeName="opacity" from="1" to="0" dur="5s" begin="2s" repeatCount="indefinite" />
-                </circle>
-            </g>
+            <!-- Primeiro Círculo -->
+            <circle cx="71.4%" cy="50%" r="30.8%" stroke="#a3ecff" stroke-width="5%" fill="none">
+                <animateTransform attributeName="transform" type="translate" from="0,0" to="-5.86%,-9.77%" dur="5s" begin="0s" repeatCount="indefinite" />
+                <animate attributeName="r" from="9.8%" to="19.5%" dur="5s" begin="0s" repeatCount="indefinite" />
+                <animate attributeName="opacity" from="1" to="0" dur="5s" begin="0s" repeatCount="indefinite" />
+            </circle>
+
+            <!-- Segundo Círculo -->
+            <circle cx="85.7%" cy="59.4%" r="24.8%" stroke="#a4e3ff" stroke-width="4.0%" fill="none">
+                <animateTransform attributeName="transform" type="translate" from="0,0" to="7.8%,-11.7%" dur="5s" begin="1s" repeatCount="indefinite" />
+                <animate attributeName="r" from="9.8%" to="23.4%" dur="5s" begin="1s" repeatCount="indefinite" />
+                <animate attributeName="opacity" from="1" to="0" dur="5s" begin="1s" repeatCount="indefinite" />
+            </circle>
+
+            <!-- Terceiro Círculo -->
+            <circle cx="80.4%" cy="67.2%" r="20.8%" stroke="#a3f9ff" stroke-width="2.6%" opacity="0.3433" fill="none">
+                <animateTransform attributeName="transform" type="translate" from="0,0" to="-3.9%,-7.8%" dur="5s" begin="2s" repeatCount="indefinite" />
+                <animate attributeName="r" from="7.8%" to="17.6%" dur="5s" begin="2s" repeatCount="indefinite" />
+                <animate attributeName="opacity" from="1" to="0" dur="5s" begin="2s" repeatCount="indefinite" />
+            </circle>
+        </g>
         `;
 
         this.svg.html(svgContent);
 
         // Adicionar o texto da medida com as propriedades de formatação
         this.svg.append("text")
-            .attr("x", "50%")
-            .attr("y", "70%")
+            .attr("x", "20%")
+            .attr("y", "28%")
             .attr("text-anchor", "middle")
             .attr("dominant-baseline", "middle")
             .attr("font-size", settings.fontSize)
